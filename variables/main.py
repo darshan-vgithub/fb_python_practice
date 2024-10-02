1deb4b44d7611ee77a5635a8d747b25c4a88e2be
Darshan.V <darshanv487@gmail.com>
2024-10-02 18:29:01 +0530

    variables/main.py/practice.py:learnt the types of data types and declaration of it and learnt about the print f

--- variables/main.py
+++ variables/main.py
@@ -11,3 +11,42 @@ email="DqC7A@example.com"
 
 print(f"Hello {first_name} your favorite food is {food} and your email is {email}")
 
+print("====================================\n")
+#Integers
+age=24
+print(f"you are {age} years old")
+
+quantity=5
+print(f"I purchased {quantity} of goldbees")
+
+num_of_students=30
+print(f"There are {num_of_students} students in the class\n")
+
+print("====================================\n")
+
+
+#Float
+price=9.99
+print(f"The price of the item is ${price}")
+
+gpa=9.5
+print(f"Your gpa is {gpa}")
+
+distance=10.5
+print(f"You have travelled {distance}km \n")
+
+print("====================================\n")
+
+
+#Boolean
+
+is_student=False
+print(f"You are a student {is_student}")
+for_sale=True
+
+if for_sale:
+    if is_student:
+        print("you cant buy the item")
+    else:
+        print("you can buy the item")
+
---
+++ variables/practice_variables.py
@@ -0,0 +1,9 @@
+name="darshan"
+age=24
+job="programmer"
+is_going_to_abroad=True
+
+if is_going_to_abroad:
+    print(f"{name} who is {age} years old and is a {job} is going to abord")
+else:
+    print(f"{name} who is {age} years old and is a {job} is not going to abord")
