f6cc9926833f4554bfab72a2bb2cb41d1433125e
Darshan.V <darshanv487@gmail.com>
2024-10-02 17:31:22 +0530

    variables/main.py:started the variables and how to declare a string variable

---
+++ variables/main.py
@@ -0,0 +1,13 @@
+# Variable= A container for a value (string, int, float,bool)
+# Strings
+first_name="Darshan"
+print(first_name) #normal printing the name 
+print(f"Hello {first_name}") # this is format we can print the variable along with string 
+
+
+# practice the f string 
+food="pizza"
+email="DqC7A@example.com"
+
+print(f"Hello {first_name} your favorite food is {food} and your email is {email}")
+
