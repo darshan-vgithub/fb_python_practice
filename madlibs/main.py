9b53fd49966c6f585b7eb178a1c77036407f6f59
Darshan.V <darshanv487@gmail.com>
2024-10-03 11:49:38 +0530

    madlibs/main.py:created a madlibs

---
+++ madlibs/main.py
@@ -0,0 +1,13 @@
+name=input("What is your name? ")
+age=int(input("How old are you? "))
+language=input("what language do you want to learn? ")
+motive=input("what is your motive? ")
+
+
+
+
+print(f"Hi I am {name}")
+print(f"I am {age} years old and I am currently learning {language}")
+print(f"while learning {language} I am coming across may errors and I am also not able to understand them")
+print(f"when the error comes we have to patiently look into the error line by line and solve it")
+print(f"sometimes it takes a lot of time and patience to solve the error so be have to be {motive}")
