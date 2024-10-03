31d4d97694f9eebda25cfa724af2964c21317824
Darshan.V <darshanv487@gmail.com>
2024-10-03 13:28:16 +0530

    learnt about arthematic operator and math module

---
+++ arthematicOperators/main.py
@@ -0,0 +1,24 @@
+friends=0
+# friends=friends+1
+friends+=1
+print(friends)
+
+sub=10
+sub-=4
+print(sub)
+
+div=friends/2
+print(div)
+div/=2
+print(div)
+
+pi=3.14
+x=-12
+y=23
+
+# result=round(pi)
+# result=abs(x)
+result=max(x,y,pi)
+result=min(x,y,pi)
+result=pow(4,8)
+print(result)
---
+++ mathmodule/main.py
@@ -0,0 +1,31 @@
+import math
+
+
+print(math.pi)
+print(math.e)
+
+print(math.sqrt(9))
+print(math.pow(2,3))
+
+num=5.6
+print(math.floor(num))
+print(math.ceil(num))
+
+print(math.factorial(5))
+
+
+# radius of the cirle 
+
+# 2pir
+
+circumference=2*math.pi*5
+print(circumference)
+
+# c=sqrt(a2+b)
+
+area=math.sqrt(pow(3,2)+4)
+print(area)
+
+
+
+
