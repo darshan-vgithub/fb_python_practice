6b54e8971ab4e22861a416fa5959c6e069750019
Darshan.V <darshanv487@gmail.com>
2024-10-03 14:03:23 +0530

    if_statements/weightconversionprogram.py:used if and else statement and did the basic math operations

---
+++ if_statements/weightconversionprogram.py
@@ -0,0 +1,12 @@
+weight=input("Enter the weight:")
+unit=input("Enter the unit (K or L):")
+
+if(unit=="K" or unit=="k"):
+    converted_weight=float(weight)*2.20462  
+    print(converted_weight)
+elif(unit=="L" or unit=="l"):
+    converted_weight=float(weight)/2.20462
+    print(converted_weight)
+else:
+    print("Invalid unit")
+    
