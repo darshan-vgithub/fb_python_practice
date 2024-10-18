138005fd297f5770543e61014db5e87e6e79d7c8
Darshan.v <darshanv487@gmail.com>
2024-10-18 11:37:55 +0530

    django/added the 404 error message if the url is something else

--- django/my_tennis_club/my_tennis_club/settings.py
+++ django/my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b&w8rrr6sy4y)#!m7s64o@f0m_($40e79l0r*b)9w!s6ykthbj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
